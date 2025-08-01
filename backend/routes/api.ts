e3300998ccb30bfe973e2bd46b8054ea9b520f7e
Erick Avalos Santiago <erick.avalos@rextie.com>
2025-08-01 11:14:10 -0500

    feat(backend:api): add try-catch to endpoint

--- backend/routes/api.ts
+++ backend/routes/api.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import { getLatestCryptoCurrencyData } from "../services/currency.service";
 const router = express.Router();
-router.get("/list-crypto-currency", getLatestCryptoCurrencyData);
+router.get("/list-crypto-currency", async (req, res) => {
+  try {
+    const data = await getLatestCryptoCurrencyData(10);
+    res.send(data);
+  } catch (error) {
+    res.status(500).send("Error fetching crypto currency data");
+  }
+});
 
 export default router;
--- backend/services/currency.service.ts
+++ backend/services/currency.service.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const headers = {
+const cryptoCurrencyHeaders = {
   "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY,
 };
 
 export async function getLatestCryptoCurrencyData(limit = 10) {
   const url = `${process.env.CMC_API_URL}/cryptocurrency/listings/latest?limit=${limit}&convert=USD`;
   console.log(`url: ${url}`);
-  const response = await axios.get(url, { headers });
+  const response = await axios.get(url, { headers: cryptoCurrencyHeaders });
   return response.data.data;
 }
